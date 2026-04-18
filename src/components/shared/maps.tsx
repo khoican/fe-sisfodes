8a7569be43a359adc2b81d54ff23500dba8ecbe8
Ibnu Khoirul Prasetyo <ibnukhoirulprasetyo@gmail.com>
2026-04-18 23:32:01 +0700

    fix: change maps from ssr to csr

--- src/components/shared/maps.tsx
+++ src/components/shared/maps.tsx
@@ -1,24 +1,56 @@
 import { cn } from '#/lib/utils'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { lazy, Suspense, useEffect, useMemo, useState } from 'react'
 
 interface MapsProps {
-    position: [number, number]
-    zoom?: number
-    className?: string
+  position: [number, number]
+  zoom?: number
+  className?: string
 }
 
-export default function Maps ({ position, zoom = 16, className }: MapsProps) {
+export default function Maps({ position, zoom = 16, className }: MapsProps) {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  const Map = useMemo(() => {
+    if (typeof window === 'undefined') return null
+
+    return {
+      Container: lazy(() => import('react-leaflet').then((m) => ({ default: m.MapContainer }))),
+      Tile: lazy(() => import('react-leaflet').then((m) => ({ default: m.TileLayer }))),
+      Marker: lazy(() => import('react-leaflet').then((m) => ({ default: m.Marker }))),
+      Popup: lazy(() => import('react-leaflet').then((m) => ({ default: m.Popup }))),
+    }
+  }, [])
+
+  if (!isClient || !Map) {
+    return (
+      <div className={cn('w-full h-full bg-slate-200 animate-pulse flex items-center justify-center', className)}>
+        <span className="text-sm text-slate-500 font-poppins">Memuat Peta Desa...</span>
+      </div>
+    )
+  }
+
   return (
-    <MapContainer className={cn('w-full h-full', className)} center={position} zoom={zoom} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <Suspense fallback={<div className={cn('w-full h-full bg-slate-100', className)} />}>
+      <Map.Container 
+        className={cn('w-full h-full', className)} 
+        center={position} 
+        zoom={zoom} 
+        scrollWheelZoom={false}
+      >
+        <Map.Tile
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Map.Marker position={position}>
+          <Map.Popup>
+            Kantor Desa Sumberkejayan. <br /> Lokasi Layanan Publik.
+          </Map.Popup>
+        </Map.Marker>
+      </Map.Container>
+    </Suspense>
   )
 }
