48eaf7807dc4c11b2ce337800b333106c654ea92
Ibnu Khoirul Prasetyo <ibnukhoirulprasetyo@gmail.com>
2026-04-19 02:17:24 +0700

    style: applied responsive style

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -10,17 +10,11 @@
 
 import { Route as rootRouteImport } from './routes/__root'
 import { Route as ProfilRouteImport } from './routes/profil'
-import { Route as AboutRouteImport } from './routes/about'
 import { Route as IndexRouteImport } from './routes/index'
 
 const ProfilRoute = ProfilRouteImport.update({
   id: '/profil',
   path: '/profil',
-  getParentRoute: () => rootRouteImport,
-} as any)
-const AboutRoute = AboutRouteImport.update({
-  id: '/about',
-  path: '/about',
   getParentRoute: () => rootRouteImport,
 } as any)
 const IndexRoute = IndexRouteImport.update({
@@ -31,31 +25,27 @@ const IndexRoute = IndexRouteImport.update({
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
-  '/about': typeof AboutRoute
   '/profil': typeof ProfilRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
-  '/about': typeof AboutRoute
   '/profil': typeof ProfilRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
-  '/about': typeof AboutRoute
   '/profil': typeof ProfilRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/about' | '/profil'
+  fullPaths: '/' | '/profil'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/about' | '/profil'
-  id: '__root__' | '/' | '/about' | '/profil'
+  to: '/' | '/profil'
+  id: '__root__' | '/' | '/profil'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
-  AboutRoute: typeof AboutRoute
   ProfilRoute: typeof ProfilRoute
 }
 
@@ -66,13 +56,6 @@ declare module '@tanstack/react-router' {
       path: '/profil'
       fullPath: '/profil'
       preLoaderRoute: typeof ProfilRouteImport
-      parentRoute: typeof rootRouteImport
-    }
-    '/about': {
-      id: '/about'
-      path: '/about'
-      fullPath: '/about'
-      preLoaderRoute: typeof AboutRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/': {
@@ -87,7 +70,6 @@ declare module '@tanstack/react-router' {
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
-  AboutRoute: AboutRoute,
   ProfilRoute: ProfilRoute,
 }
 export const routeTree = rootRouteImport
--- src/routes/profil.tsx
+++ src/routes/profil.tsx
@@ -15,18 +15,18 @@ function Profil () {
   return (
     <main className=''>
       <section className='grid md:grid-cols-8 gap-10 items-center bg-white px-4 lg:px-12 pb-8 pt-8'>
-        <div className='col-span-5'>
+        <div className='w-full md:col-span-5'>
           <h1 className='text-6xl font-extrabold'>
             Mengenal <span className='text-primary'>Luhurnya</span> Sejarah Kami
           </h1>
-          <p className='text-lg text-gray-600 mt-4'>
+          <p className='text-sm md:text-lg text-gray-600 mt-4'>
             Desa Sumberkejayan bukan sekadar titik geografis, melainkan warisan
             peradaban yang terus tumbuh dalam harmoni antara nilai leluhur dan
             modernitas digital.
           </p>
         </div>
 
-        <div className='col-span-3 h-[60vh] relative rounded-2xl overflow-hidden'>
+        <div className='w-full md:col-span-3 h-[60vh] relative rounded-2xl overflow-hidden'>
           <Image
             src='https://images.unsplash.com/photo-1584773920278-4ada215dbe93?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             alt='Desa Sumberkejayan'
@@ -41,7 +41,7 @@ function Profil () {
       <section className='mt-16 px-4 lg:px-12'>
         <Title title='Sejarah Desa' />
 
-        <div className='grid grid-cols-3 gap-6 mt-6'>
+        <div className='grid md:grid-cols-3 gap-6 mt-6'>
           <Image
             src='https://images.unsplash.com/photo-1716731049987-c5ec344a9c80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZGVzYSUyMGluZG9uZXNpYXxlbnwwfHwwfHx8MA%3D%3D'
             alt='Desa Sumberkejayan'
@@ -49,7 +49,7 @@ function Profil () {
             layout='fullWidth'
           />
 
-          <div className='col-span-2 bg-white rounded-2xl shadow p-8 border-b-6 border-primary'>
+          <div className='w-full md:col-span-2 bg-white rounded-2xl shadow p-8 border-b-6 border-primary'>
             <h3 className='text-xl font-semibold text-primary'>Sejarah Awal</h3>
             <p
               className='flex flex-col gap-4 text-gray-500 mt-4 leading-relaxed'
@@ -62,17 +62,17 @@ function Profil () {
       <section className='px-4 lg:px-12 bg-primary mt-24 relative h-full overflow-hidden py-12'>
         <div className='h-[300%] bg-white/20 -rotate-25 w-40 absolute -top-30 -right-10 z-0'></div>
 
-        <div className='text-white grid grid-cols-2 gap-8'>
+        <div className='text-white grid lg:grid-cols-2 gap-8'>
           <div>
             <p className='text-xs tracking-wider text-white/80'>
               Cita-Cita Luhur
             </p>
 
-            <h3 className='text-8xl font-extrabold tracking-wider mt-8'>
+            <h3 className='text-4xl lg:text-8xl font-extrabold tracking-wider mt-8'>
               VISI
             </h3>
 
-            <p className='text-2xl italic mt-6'>"{vision.content}"</p>
+            <p className='text-lg md:text-2xl italic mt-6'>"{vision.content}"</p>
           </div>
           <div>
             <h3 className='text-4xl font-extrabold tracking-wider'>MISI</h3>
@@ -99,12 +99,12 @@ function Profil () {
         <h1 className='text-4xl font-semibold text-center'>
           Struktur Organisasi
         </h1>
-        <p className='text-center text-gray-600 mt-4 text-sm w-1/3 mx-auto'>
+        <p className='text-center text-gray-600 mt-4 text-sm w-full md:w-1/3 mx-auto'>
           Sinergi antara pemimpin berpengalaman dan tenaga muda progresif untuk
           melayani masyarakat Sumberkejayan.
         </p>
 
-        <div className='w-1/3 mx-auto mt-16'>
+        <div className='w-full md:w-1/3 mx-auto mt-16'>
           {leader && <PersonCard {...leader} />}
         </div>
 
