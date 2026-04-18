3c3d46132205a331a3a414f8727489d73ef446d4
Ibnu Khoirul Prasetyo <ibnukhoirulprasetyo@gmail.com>
2026-04-18 23:17:53 +0700

    feat: deploy to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
+import { defineConfig } from 'vite'
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(), netlify()],
 })
 
 export default config
